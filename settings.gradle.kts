ea422b7a460039f6129de5379e70afdb3d3e1153
Egil Ballestad <egil.ballestad@vigoiks.no>
2026-01-06 12:30:00 +0100

    refactor and add override method for process instance

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,13 @@
+pluginManagement {
+    repositories {
+        maven {
+            url = uri("https://repo.fintlabs.no/releases")
+        }
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
