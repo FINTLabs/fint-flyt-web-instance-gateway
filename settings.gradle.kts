2a63a64f0e0f398e4465d85c2c840c527f2ca169
Egil Ballestad <egil.ballestad@vigoiks.no>
2026-01-06 12:34:40 +0100

    refactor and add override method for process instance

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,1 @@
-pluginManagement {
-    repositories {
-        maven {
-            url = uri("https://repo.fintlabs.no/releases")
-        }
-        gradlePluginPortal()
-        mavenCentral()
-    }
-}
-
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
 rootProject.name = "flyt-web-instance-gateway"
