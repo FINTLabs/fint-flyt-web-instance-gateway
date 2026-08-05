457ec8d8f67586c13265f39851a36bd342abc9dc
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2026-08-05 13:34:35 +0200

    feat: oppgrader til web-resource-server 4.0.0-rc-2

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-parameter-names")
     implementation("org.apache.httpcomponents.client5:httpclient5")
 
-    api("no.novari:flyt-web-resource-server:4.0.0-rc-1")
+    api("no.novari:flyt-web-resource-server:4.0.0-rc-2")
     api("no.novari:flyt-kafka:7.2.0")
 
     testImplementation(kotlin("test"))
