ff3424c0a7aa49a359af9376b25dff0e801cf263
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2026-08-12 12:50:59 +0200

    fix: bump no.novari:flyt-web-resource-server til 4.1.0-rc-1
    flyt-web-resource-server eksponerer no.novari:kafka som api-avhengighet.
    Uten denne bumpen ville den gamle 6.2.0-versjonen (uten sporingsbryteren
    fra FFS-2221) fortsatt kommet inn via denne stien ved siden av den
    allerede bumpede flyt-kafka-avhengigheten.

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-parameter-names")
     implementation("org.apache.httpcomponents.client5:httpclient5")
 
-    api("no.novari:flyt-web-resource-server:4.0.0")
+    api("no.novari:flyt-web-resource-server:4.1.0-rc-1")
     api("no.novari:flyt-kafka:7.3.0-rc-1")
 
     testImplementation(kotlin("test"))
