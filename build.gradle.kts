b77c54b7fbc0f68c9d86eee23da96ca1705415af
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2026-08-12 13:54:23 +0200

    fix: bump flyt-kafka og flyt-web-resource-server til nyeste RC
    Følger property-navnerettelsen i fint-kafka (novari.kafka.tracing.enabled).

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,8 +50,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-parameter-names")
     implementation("org.apache.httpcomponents.client5:httpclient5")
 
-    api("no.novari:flyt-web-resource-server:4.1.0-rc-1")
-    api("no.novari:flyt-kafka:7.3.0-rc-1")
+    api("no.novari:flyt-web-resource-server:4.1.0-rc-2")
+    api("no.novari:flyt-kafka:7.3.0-rc-2")
 
     testImplementation(kotlin("test"))
     testImplementation("org.springframework.boot:spring-boot-starter-test")
